d291f2c2c3960d094a01d9a84bc98ca2630e5c64
Giovanni Putignano <gputignano@gmail.com>
2025-04-22 13:51:54 +0200

    Several updates

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// import WebSocket from "ws";
+import axios from "axios";
 import createBinanceSocket from "./src/utils/webSocket.js";
-import { WEBSOCKET_STREAM_ENDPOINT, WEBSOCKET_API_ENDPOINT, ED25519_API_KEY, ED25519_PRIVATE_KEY } from "./src/config/config.js";
+import { WEBSOCKET_STREAM_ENDPOINT, WEBSOCKET_API_ENDPOINT, ED25519_API_KEY, ED25519_PRIVATE_KEY, WEB_APP_URL } from "./src/config/config.js";
 import { sessionLogon } from "./src/utils/functions.js";
 
 const symbol = {
@@ -12,26 +12,32 @@ const min_notional = 5;
 const price_filter = 0.01000000;
 const lot_size = 0.00001000;
 let isProcessing = false;
+
+const response = await axios.get(WEB_APP_URL);
+
 const last_trade = {
-  side: null,
-  time: null,
-  price: 0,
-  price_executed: 0,
-  wallet_target: 1000, // Set initial value
-  base_required: 0,
-  base_to_buy: 0,
-  base_to_buy_fee: 0,
-  base_owned: 0, // Set initial value
-  quote_spent: 0,
-  quote_spent_fee: 0,
-  quote_spent_sum: 0, // Set initial value
-  delta: 0,
-  buffer: 0 // Set initial value
+  side: response.data[0],
+  time: response.data[1],
+  id: response.data[2],
+  price: response.data[3],
+  price_executed: response.data[4],
+  wallet_target: response.data[5], // Set initial value
+  base_required: response.data[6],
+  base_to_buy: response.data[7],
+  base_to_buy_fee: response.data[8],
+  base_owned: response.data[9], // Set initial value
+  quote_spent: response.data[10],
+  quote_spent_fee: response.data[11],
+  quote_spent_sum: response.data[12], // Set initial value
+  delta: response.data[13],
+  buffer: response.data[14] // Set initial value
 };
+
+console.log(last_trade);
 
 const ws_api = createBinanceSocket(WEBSOCKET_API_ENDPOINT);
 
-ws_api.on("close", (code, reason) => console.log(code, reason.toString(), "ws_api closed"));
+ws_api.on("close", (code, reason) => console.log(code, reason?.toString(), "ws_api closed"));
 
 ws_api.on("open", () => {
   console.log("ws_api: open");
@@ -41,7 +47,7 @@ ws_api.on("open", () => {
 
 ws_api.on("error", (error) => console.error(error));
 
-ws_api.on("message", (data) => {
+ws_api.on("message", async (data) => {
   const parsedData = JSON.parse(data);
 
   switch (parsedData.id) {
@@ -67,84 +73,8 @@ ws_api.on("message", (data) => {
       // 
       break;
     case "account_status":
-      // const btcBalance = parsedData.result.balances.filter(row => row.asset === "BTC")[0].free;
-
-      // ws_api.send(JSON.stringify({
-      //   id: "order_place",
-      //   method: "order.place",
-      //   params: {
-      //     symbol: `${symbol.base}${symbol.quote}`,
-      //     side: "SELL",
-      //     type: "MARKET",
-      //     quantity: btcBalance,
-      //     timestamp: Date.now()
-      //   }
-      // }));
-
-      const ws_stream = createBinanceSocket(WEBSOCKET_STREAM_ENDPOINT);
-
-      ws_stream.on("close", (code, reason) => console.log(code, reason.toString(), "ws_stream closed"));
-
-      ws_stream.on("open", () => {
-        console.log("ws_stream: open");
-
-        ws_stream.send(JSON.stringify({
-          "method": "SUBSCRIBE",
-          "params": [
-            "btcusdc@aggTrade"
-          ],
-          "id": "subscribe"
-        }));
-      });
-
-      ws_stream.on("error", (error) => console.error(error));
-
-      ws_stream.on("message", (data) => {
-        const parsedData = JSON.parse(data);
-
-        if (parsedData.result === null) return;
-
-        switch (parsedData.e) {
-          case "aggTrade":
-
-            if (isProcessing) return;
-
-            const price = parseFloat(parsedData.p);
-
-            const wallet_target = last_trade.wallet_target + wallet_increment;
-            const base_required = wallet_target / price;
-            const base_to_buy = parseFloat((Math.round((base_required - last_trade.base_owned) / lot_size) * lot_size).toFixed(-Math.log10(lot_size)));
-            const side = base_to_buy > 0 ? "BUY" : "SELL";
-            const quote_to_buy = base_to_buy * price;
-
-            if (Math.abs(quote_to_buy) >= min_notional && Math.abs((price - last_trade.price) / last_trade.price) >= 0.01) {
-              // Execute the order
-
-              isProcessing = true;
-              const trade = {
-                id: "order_place",
-                method: "order.place",
-                params: {
-                  symbol: `${symbol.base}${symbol.quote}`,
-                  side: side,
-                  type: "LIMIT",
-                  timeInForce: "FOK",
-                  price: price,
-                  quantity: Math.abs(base_to_buy),
-                  timestamp: Date.now()
-                }
-              };
-
-              ws_api.send(JSON.stringify(trade));
-
-            }
-
-            break;
-          default:
-            //
-            break;
-        }
-      });
+      //
+      break;
     default:
       break;
   }
@@ -171,10 +101,27 @@ ws_api.on("message", (data) => {
         last_trade.delta = wallet_increment - last_trade.quote_spent;
         last_trade.buffer += last_trade.delta;
 
-        isProcessing = false;
+        const response = await axios.post(WEB_APP_URL, {
+          side: last_trade.side,
+          time: last_trade.time,
+          id: trade.i,
+          price: last_trade.price,
+          price_executed: last_trade.price_executed,
+          wallet_target: last_trade.wallet_target,
+          base_required: last_trade.base_required,
+          base_to_buy: last_trade.base_to_buy,
+          base_to_buy_fee: last_trade.base_to_buy_fee,
+          base_owned: last_trade.base_owned,
+          quote_spent: last_trade.quote_spent,
+          quote_spent_fee: last_trade.quote_spent_fee,
+          quote_spent_sum: last_trade.quote_spent_sum,
+          delta: last_trade.delta,
+          buffer: last_trade.buffer
+        });
 
-        // console.log(parsedData, last_trade);
-        console.log(`${last_trade.side},${last_trade.price_executed},${last_trade.base_to_buy},${last_trade.base_owned},${last_trade.quote_spent},${last_trade.quote_spent_sum},${last_trade.delta},${last_trade.buffer}`);
+        console.log(response.data);
+
+        isProcessing = false;
 
         return;
       } else if (parsedData.event.X === "EXPIRED") {
@@ -184,6 +131,70 @@ ws_api.on("message", (data) => {
 
       break;
     default:
+      break;
+  }
+});
+
+const ws_stream = createBinanceSocket(WEBSOCKET_STREAM_ENDPOINT);
+
+ws_stream.on("close", (code, reason) => console.log(code, reason?.toString(), "ws_stream closed"));
+
+ws_stream.on("open", () => {
+  console.log("ws_stream: open");
+
+  ws_stream.send(JSON.stringify({
+    "method": "SUBSCRIBE",
+    "params": [
+      "btcusdc@aggTrade"
+    ],
+    "id": "subscribe"
+  }));
+});
+
+ws_stream.on("error", (error) => console.error(error));
+
+ws_stream.on("message", (data) => {
+  const parsedData = JSON.parse(data);
+
+  if (parsedData.result === null) return;
+
+  switch (parsedData.e) {
+    case "aggTrade":
+
+      if (isProcessing) return;
+
+      const price = parseFloat(parsedData.p);
+
+      const wallet_target = last_trade.wallet_target + wallet_increment;
+      const base_required = wallet_target / price;
+      const base_to_buy = parseFloat((Math.round((base_required - last_trade.base_owned) / lot_size) * lot_size).toFixed(-Math.log10(lot_size)));
+      const side = base_to_buy > 0 ? "BUY" : "SELL";
+      const quote_to_buy = base_to_buy * price;
+
+      if (Math.abs(quote_to_buy) >= min_notional && Math.abs((price - last_trade.price) / last_trade.price) >= 0.01) {
+        // Execute the order
+
+        isProcessing = true;
+        const trade = {
+          id: "order_place",
+          method: "order.place",
+          params: {
+            symbol: `${symbol.base}${symbol.quote}`,
+            side: side,
+            type: "LIMIT",
+            timeInForce: "FOK",
+            price: price,
+            quantity: Math.abs(base_to_buy),
+            timestamp: Date.now()
+          }
+        };
+
+        ws_api.send(JSON.stringify(trade));
+      }
+
+      break;
+    default:
+      //
       break;
   }
 });
